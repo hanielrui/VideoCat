09e923ba8fa808da78a0db35f336b321be1c18ec
hanielrui <76834780+hanielrui@users.noreply.github.com>
2026-03-30 19:01:15 +0800

    Update JellyfinHomeViewController.swift

--- VideoPlayer/Features/JellyfinHomeViewController.swift
+++ VideoPlayer/Features/JellyfinHomeViewController.swift
@@ -212,7 +212,7 @@ class JellyfinHomeViewController: BaseViewController {
     }
 
     // MARK: - UITableViewDelegate
-    override func tableView(
+    func tableView(
         _ tableView: UITableView,
         didSelectRowAt indexPath: IndexPath
     ) {
